bb701a6f47ea13f838e510fd7e086c5d241b14c8
sealanguage <elaine.froehlich@gmail.com>
2020-06-10 17:27:56 -0400

    working on algorithms to compare numbers in sets

---
+++ compare_ints_in_sets.py
@@ -0,0 +1,11 @@
+
+#comapring ints in lists
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+a = input()
+
+lis = a.split()
+
+for line in lis :
+    print (line)
