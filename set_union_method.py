2806647e9d3d2f695131e4f9c208e007f655ea0d
sealanguage <elaine.froehlich@gmail.com>
2020-06-15 22:09:46 -0400

    learning with set.union() method in python

---
+++ set_union_method.py
@@ -0,0 +1,5 @@
+odd_num1 = {1, 3, 5, 7}
+odd_num2 = {7, 9, 11, 13}
+
+odd = odd_num1.union(odd_num2)
+odd
---
+++ set_union_pipe.py
@@ -0,0 +1,5 @@
+odd_num1 = {1, 3, 5, 7, 9}
+odd_num2 = {7, 9, 11, 13}
+
+odd = odd_num1 | odd_num2
+odd
