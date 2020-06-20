9b9aca3633ff02c95b9de25e8a2d7762c3130727
sealanguage <elaine.froehlich@gmail.com>
2020-06-20 16:35:14 -0400

    calculate meal cost not passing 4 of 4 test cases.

--- calculate_meal_cost_tip_tax.py
+++ calculate_meal_cost_tip_tax.py
@@ -7,24 +7,18 @@ import re
 import sys
 
 # Complete the solve function below.
+
 def round_down(n, decimals=0):
     multiplier = 10 ** decimals
     return math.floor(n * multiplier) / multiplier
 
 def solve(meal_cost, tip_percent, tax_percent):
-    tip = meal_cost * (tip_percent/100)
-    tip = round(tip, 1)
-    tax = meal_cost * (tax_percent/100)
+    tip = round(meal_cost * (tip_percent/100.0))
+    tax = meal_cost * (tax_percent/100.0)
     tax = round(tax, 2)
-    # print(meal_cost, tip_percent, tax_percent)
-    # print(tip, tax)
 
-    total_cost = round_down(meal_cost + tip + tax)
-    # round_down(total_cost)
-    print(total_cost)
-
-
-    
+    total_cost = meal_cost + tip + tax
+    print(round(total_cost))
 
 
 if __name__ == '__main__':
