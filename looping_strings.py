d976690eb842433a616addc418ceb538e01d3862
sealanguage <elaine.froehlich@gmail.com>
2020-06-27 23:24:26 -0400

    working on finding letters in words by even and odd indexes.

---
+++ looping_strings.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    T = int(input())
+    S = input()
+    string = S
+
+# print(len(S))
+# for letter in range(S):
+#         print(letter)
+
+
+for word in string :
+    # print(word)
+    
+    for letter in word :
+        print(letter)
