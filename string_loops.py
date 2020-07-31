02701051552228061f3e42e3603ea2874a2b1d13
lane froehlich <elaine.froehlich@gmail.com>
2020-07-30 21:07:28 -0400

    working on looping through strings in python. just discovered string operators. looks promising. still only get one word of a 2 word string

--- string_loops.py
+++ string_loops.py
@@ -1,9 +1,26 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
 
 if __name__ == '__main__':
     T = int(input())
     S = input()
+    string = S
+
+# print(len(S))
+# for letter in range(S):
+#         print(letter)
+
+print("T is: ", T)
+print("S is: ", S)
+
+# Slicing Operator
+# string [starting index : ending index : step value]
+# for char in string_to_iterate[0 : 6 : 1]:
+#    print(char)
 
 
-for letter in S :
-
-    print(T, letter)
+for word in string:
+    print(word)
+    
+    for letter in [ : : 2] :
+        print(letter, end = '')
