9fe25e5d4c8667bb9c00a7209f88968175a4f1b1
lane froehlich <elaine.froehlich@gmail.com>
2020-08-05 23:07:01 -0400

    working to loop over a string algorithm. looping using splice [] operator iterates over the first word, separates every other letter and iterates over every other letter the second time. need to see second word and add spaces between words on print statement

--- string_loops.py
+++ string_loops.py
@@ -1,26 +1,19 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     T = int(input())
     S = input()
     string = S
+    print(T)
+    # for letter in range(S):
+    #         print(letter)
 
-# print(len(S))
-# for letter in range(S):
-#         print(letter)
+    # for word in T :
+    # print(word)
 
-print("T is: ", T)
-print("S is: ", S)
+    # use slice [] operator to iterate every other letter
+    for letter in string[::2]:
+        print(letter, end="")
+    # use slice [] operator to get the missing letters
+    for letter in string[1::2]:
 
-# Slicing Operator
-# string [starting index : ending index : step value]
-# for char in string_to_iterate[0 : 6 : 1]:
-#    print(char)
+        print(letter, end="")
 
-
-for word in string:
-    print(word)
-    
-    for letter in [ : : 2] :
-        print(letter, end = '')
