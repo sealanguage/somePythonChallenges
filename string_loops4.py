672337a86e337f89a4333f5aa58157c359034c42
lane froehlich <elaine.froehlich@gmail.com>
2020-08-20 20:42:18 -0400

    finally, I can console print both strings. I'm not sure how it works but I should be able to start to pull all this together.

---
+++ string_loops4.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+
+    # a, b = input("Enter two of your lucky number: ").split()
+    T = int(input())
+    S = input()
+
+    splits = input().split()
+
+    print("The original string is : " + S)
+
+    # while (splits < T):
+    print("splits is ", splits)
+
+    # trying to get the second string in the input-not working
+    for split in splits:
+        print("split is: ", split)
+
+    # use slice [] operator to iterate every other letter
+    for letter in split[::2]:
+        print(letter, end="")
+    # use slice [] operator to get the missing letters
+    print(end=" ")
+    for letter in split[1::2]:
+        print(letter, end="")
