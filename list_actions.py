5a9546caeb5ea4bd9ca2208590cf6a4317066b91
lane froehlich <elaine.froehlich@gmail.com>
2020-08-21 21:02:00 -0400

    reading commands and values from a list to prerform the list commands on the values read from the list. the input is correctly reading in as smaller lists. list_actions.py

---
+++ list_actions.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    N = int(input())
+    #  print("number of lines ", N)
+
+    list = []
+
+    for i in range(N):
+        print(input().split())  #  prints each line of the input
