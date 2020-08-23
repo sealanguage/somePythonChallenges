bb818c21d78d3339504bccaf32ab2bc374134be5
lane froehlich <elaine.froehlich@gmail.com>
2020-08-22 23:02:43 -0400

    trying to read the contents of the input as separate lists. it's repeating in some weird way right now

--- list_actions.py
+++ list_actions.py
@@ -2,7 +2,9 @@ if __name__ == "__main__":
     N = int(input())
     #  print("number of lines ", N)
 
-    list = []
+    mylist = []
 
     for i in range(N):
-        print(input().split())  #  prints each line of the input
+        # print(input().split())  #  prints each line of the input
+        mylist.append(input().split())
+        print(mylist)
