a416cbcd8a26d10943df762c45a115fe629ec5cc
lane froehlich <elaine.froehlich@gmail.com>
2020-08-24 17:30:25 -0400

    working on reading input into a list of lists(I think). Here I am separating the list into N sublists of strings.

--- list_actions.py
+++ list_actions.py
@@ -1,9 +1,17 @@
 if __name__ == "__main__":
-    N = int(input())
+    if __name__ == "__main__":
+        N = int(input())
     #  print("number of lines ", N)
+    mylist = []
 
-    #  this separates each line of input into single list
     for i in range(N):
-        mylist = []
-        mylist.append(input().split())
-        print(mylist)
+        inlist = []
+        #  this successfully prints the first element in each list
+        inlist.append(input().split())
+        # print("inlist[0][0] ", inlist[0][0])   # returns position 0 - 12 lines
+        # print("inlist[0][1] ", inlist[0][1])   # returns position 1 - till print
+        print("inlist[0][2] ", inlist[0][2])  # returns position 2 - till print
+        # print(inlist[i][1] + inlist[i][2])
+        # if inlist[0][1] == 0:
+        #     print("zero")
+        # # print(inlist)
