0b72063804f96830afe263af7dd53aa8d71dcc94
lane froehlich <elaine.froehlich@gmail.com>
2020-08-27 20:54:04 -0400

    working on reading the individual lines as list. this code where I ask for mylist[0] prints the first line 12 times (N). now to iterate through mylist[0] to get the 3 values.

--- list_actions.py
+++ list_actions.py
@@ -1,6 +1,5 @@
 if __name__ == "__main__":
-    if __name__ == "__main__":
-        N = int(input())
+   N = int(input())
     #  print("number of lines ", N)
     mylist = []
 
@@ -10,7 +9,15 @@ if __name__ == "__main__":
         inlist.append(input().split())
         # print("inlist[0][0] ", inlist[0][0])   # returns position 0 - 12 lines
         # print("inlist[0][1] ", inlist[0][1])   # returns position 1 - till print
-        print("inlist[0][2] ", inlist[0][2])  # returns position 2 - till print
+        # print("inlist[0][2] ", inlist[0][2])   # returns position 2 - till print  
+        mylist.append(inlist[0])
+        # if mylist[0][0] == print:
+        print(mylist[0])
+
+        # list methods append() , extend() , and insert() to add items to a list
+
+
+
         # print(inlist[i][1] + inlist[i][2])
         # if inlist[0][1] == 0:
         #     print("zero")
