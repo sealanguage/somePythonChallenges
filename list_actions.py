8305cae009871139f04fe47a7403f16fd40c7fc2
lane froehlich <elaine.froehlich@gmail.com>
2020-08-31 18:22:37 -0400

    had to back up to get the loop to work again. it loops through the first set of inputs until it gets to the print input. now to get the values to become an actual command.

--- list_actions.py
+++ list_actions.py
@@ -2,24 +2,20 @@ if __name__ == "__main__":
     N = int(input())
     #  print("number of lines ", N)
     mylist = []
-    endlist = []
+    # endlist = []
+    mylist.insert(0, 5)  # insert int 5 at position 0, this is how insert works
+    print("mylist.insert: ", mylist)
 
     for i in range(N):
         inlist = []
+
         #  this successfully prints the first element in each list
         inlist.append(input().split())
-        # print("inlist[0][0] ", inlist[0][0])   # returns position 0 - 12 lines
-        # print("inlist[0][1] ", inlist[0][1])   # returns position 1 - till print
-        # print("inlist[0][2] ", inlist[0][2])   # returns position 2 - till print
-        # mylist.append(inlist[0])
-        # if mylist[0][0] == print:
-        print(inlist[0])
-        # print(inlist[0][0])
-        # print(inlist[0][1])
-        # print(inlist[0][2])
-        endlist[0].insert(inlist[0][5])
-
-        print(endlist)
+        # mylist.insert(inlist[0][1], inlist[0][2])
+        print("inlist[0][0] ", inlist[0][0])  # returns position 0 - 12 lines
+        print("inlist[0][1] ", inlist[0][1])  # returns position 1 - till print
+        print("inlist[0][2] ", inlist[0][2])  # returns position 2 - till print
+        mylist.append(inlist[0])
 
         # list methods append() , extend() , and insert() to add items to a list
 
