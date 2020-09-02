005332c07720e08c3b841479b4a31bf897001f02
lane froehlich <elaine.froehlich@gmail.com>
2020-09-02 12:33:39 -0400

    completed algorithm reads input list, adds, removes, sorts, reverses the list based on input from the user.

--- list_actions.py
+++ list_actions.py
@@ -3,31 +3,23 @@ if __name__ == "__main__":
     #  print("number of lines ", N)
     mylist = []
     testlist = []
-    # testlist.insert(0, 5)   # insert int 5 at position 0, this is how insert works
+    # testlist.insert(0, 5)   # insert int 5 at position 0, this is insert works
     # testlist.insert(1, 10)
     # testlist.insert(0, 6)
-
-    # print("testlist.insert: ", testlist)
 
     for i in range(N):
         inlist = []
 
         #  this successfully prints the first element in each list
         inlist.append(input().split())
-        # mylist.insert(inlist[0][1], inlist[0][2])
 
         # # print("inlist[0][0] ", inlist[0][0])   # returns position 0 - 12 lines
         # print(inlist[0][0])   # returns position 0 - 12 lines
         # # print("inlist[0][1] ", inlist[0][1])   # returns position 1 - till print
-        # print(inlist[0][1])
         # # print("inlist[0][2] ", inlist[0][2])   # returns position 2 - till print
-        # print(inlist[0][2])
-        # mylist.append(inlist[0])
 
         if inlist[0][0] == "insert":
-            # mylist.inlist[0][0](inlist[0][1], inlist[0][2])
             mylist.insert(int(inlist[0][1]), int(inlist[0][2]))
-            # print(mylist)
         if inlist[0][0] == "print":
             print(mylist)
         if inlist[0][0] == "remove":
@@ -36,8 +28,7 @@ if __name__ == "__main__":
             mylist.append(int(inlist[0][1]))
         if inlist[0][0] == "sort":
             mylist.sort()
+        if inlist[0][0] == "reverse":
+            mylist.reverse()
         if inlist[0][0] == "pop":
             mylist.pop()
-        if inlist[0][0] == "reverse":
-            mylist.sort(reverse=True)
-
