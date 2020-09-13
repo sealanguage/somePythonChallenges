cea81526993de7c7d287f516db4b87f172edcdb8
lane froehlich <elaine.froehlich@gmail.com>
2020-09-12 22:36:11 -0400

    working to iterate through input strings in python. I can do it manually but I can't seem to iterate through the input strings.

---
+++ string_loops5.py
@@ -0,0 +1,39 @@
+ # a, b = input("Enter two of your lucky number: ").split()
+    T = int(input())
+    S = input()
+
+#  loop grab input run it and then grab the next input
+    splits = S.split()
+    print("splits :", splits)
+    # for split in splits:
+    #     print("split :", split)
+
+    print("The original string is : " + S)    # Hacker
+
+    S = input()
+# #  loop grab input run it and then grab the next input
+    # splits = S.split()
+
+    print("The original string is : " + S)    # Rank
+
+    # for split in splits:
+    #     print("this ", splits)
+        
+   
+
+    # while (splits < T):
+    # print("split is ", split)
+
+    # trying to get the second string in the input-not working
+    # for split in S.split():
+    #     print("split is: ", split)
+
+
+
+    # # use slice [] operator to iterate every other letter
+    # for letter in split[::2]:
+    #     print(letter, end="")
+    # # use slice [] operator to get the missing letters
+    # print(end=" ")
+    # for letter in split[1::2]:
+    #     print(letter, end="")
