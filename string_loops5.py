3d3d296fef54be5e7995c61fe7858fe32d4784bc
lane froehlich <elaine.froehlich@gmail.com>
2020-09-15 18:44:31 -0400

    still not able to iterate throught the user input. may be getting closer.

--- string_loops5.py
+++ string_loops5.py
@@ -1,21 +1,55 @@
  # a, b = input("Enter two of your lucky number: ").split()
     T = int(input())
-    S = input()
+    S = input()   # S is a string of chars, it prints each letter in a word
+    splits = S.split()
+    print("1 ", splits)
+    lines = []
+
+    # lines = input().strip()
+    # print("2 ", lines)
+
+    # while True:
+    #     line = S.split()
+    #     if line:
+    #         lines.append(line)
+    #     else:
+    #         break
+    # FinalText = '\n'.join(line)
+    # print("\n")
+    # print(lines)
+    
+    # str.splitlines()
+    # lines = 0
+    # Ssplit = S.split()
+    # while lines < T:
+    #     for word in Ssplit: 
+    #         print (word)
+    #         lines +=1
+
+
+    # for words in S.input().split(): 
+    #     # words = S.splitlines()
+    #     print(words)
+
+    # my_string = "this is a string"
+    # for word in my_string.split():
+    #     print (word)
 
 #  loop grab input run it and then grab the next input
-    splits = S.split()
-    print("splits :", splits)
-    # for split in splits:
-    #     print("split :", split)
+#     splits = S.split()
+#     print("splits :", splits)
+#     # for split in splits:
+#     #     print("split :", split)
 
-    print("The original string is : " + S)    # Hacker
+#     print("The original string is : " + S)    # Hacker
 
-    S = input()
-# #  loop grab input run it and then grab the next input
-    # splits = S.split()
+#     S = input()
+# # #  loop grab input run it and then grab the next input
+#     # splits = S.split()
 
-    print("The original string is : " + S)    # Rank
-
+#     print("The original string is : " + S)    # Rank
+    
+    
     # for split in splits:
     #     print("this ", splits)
         
@@ -29,7 +63,7 @@
     #     print("split is: ", split)
 
 
-
+#  keep below, it works to separate the strings
     # # use slice [] operator to iterate every other letter
     # for letter in split[::2]:
     #     print(letter, end="")
