24e7f3a404d90a176f7fae067c7a7b0428c51ee8
lane froehlich <elaine.froehlich@gmail.com>
2020-09-16 22:05:31 -0400

    comparing scores in a dictionary to list students who have 2nd lowest scores.

---
+++ finding_2nd_lowest_scores.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+
+        python_students = []
+        # print(name)
+        # print(score)
+
+        low_score = 100
+        if low_score >= score:
+            score == low_score
+            print("score ", score)
+            print("low_score ", low_score)
+
