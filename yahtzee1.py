eb1be1cfbcfef9f702ae25dc998031ff33275a1c
lane froehlich <elaine.froehlich@gmail.com>
2020-09-19 22:42:37 -0400

    started to code a yahtzee game. can roll any number of die and get random value between 1 and 6

--- yahtzee1.py
+++ yahtzee1.py
@@ -1,3 +1,11 @@
 import random
 
-print random.randint(0, 5)
+# print random.randint(0, 5)    # prints a random value, a roll of the die
+num_die = int(input("how many die?"))
+dice = []
+
+for face in range(num_die):
+    die = random.randint(1, 6)
+    dice.append(die)
+print(dice)
+
