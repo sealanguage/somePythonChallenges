36c68afee2d28bcae31daeb9b8ad611b59c6ed6b
lane froehlich <elaine.froehlich@gmail.com>
2020-09-20 20:31:54 -0400

    yahtzee game in python, can roll a number of die and get random results, working on rerolling specified die and replacind values in initial random roll list.

--- yahtzee1.py
+++ yahtzee1.py
@@ -1,11 +1,20 @@
 import random
+from operator import setitem
 
-# print random.randint(0, 5)    # prints a random value, a roll of the die
-num_die = int(input("how many die?"))
-dice = []
+# numdie = int(input("How many die to roll?"))
+numdie = 5
+dice = []  # limit to 5 max dice
+# roll = random.randint(1, 5)
 
-for face in range(num_die):
-    die = random.randint(1, 6)
-    dice.append(die)
-print(dice)
+for roll in range(numdie):
+    roll = random.randint(1, 6)
+    dice.append(roll)
+print("First roll: ", dice)
 
+#  reroll just 1st die
+reroll_die = []
+reroll = int(input("Die to reroll: "))
+
+# operator.setitem(a, b, c)    Set the value of a at index b to c.
+dice.insert(reroll, roll)  #  (i, elem) i = index   elem = replace with this
+print("dice ", dice)
