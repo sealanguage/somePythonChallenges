f7d4e1f0a69f136384af086c081ce352dcf1d557
lane froehlich <elaine.froehlich@gmail.com>
2020-10-07 21:30:39 -0400

    replacing a list item at index position with a random number with 2 digits to confirm it has been changed. replace_list_item.py

--- replace_list_item.py
+++ replace_list_item.py
@@ -8,7 +8,8 @@ change = int(input("number change at index : "))
 list[change] = random.randint(10, 15)  # randint(low value, high value)
 print("new list: ", list)
 
-change_many = int(input("number change at index separate with space : "))
+change_many = int(input("change number at index - separate with space: "))
 
-list[change_many] = random.randint(10, 15)
+for index in list:
+    list[change_many] = random.randint(10, 15)
 print("multi change list: ", list)
